fbb67a7e10759bc15df65d17a33cf62bde9d64f8
Anup Narkhede <anupnarkhede@gmail.com>
2010-08-08 14:43:43 +0100

    length validations are optional

--- test/public/tests/validations/length.js
+++ test/public/tests/validations/length.js
@@ -1,3 +1,5 @@
+module("MooModel.Validations.Length");
+
 test("It should validate length for a particular value", function(){
   var Person = new Class({
     Extends: MooModel.Base,
@@ -71,17 +73,37 @@ test("It should validate length within a given range", function(){
 });
 
 test("It should optionally validate length using minimum", function(){
-  same(true, false)
+  var Person = new Class({
+    Extends: MooModel.Base,
+    Validations: [{ attribute: 'name', type: 'length', minimum: 10, allow_null: true }]
+  });
+
+  person = new Person({id: 1, name: 'Attila'});
+  same(person.valid(), false);
+
+  person = new Person({id: 1, name: null});
+  same(person.valid(), true);
 });
 
 test("It should optionally validate length using maximum", function(){
-  same(true, false)
+  var Person = new Class({
+    Extends: MooModel.Base,
+    Validations: [{ attribute: 'name', type: 'length', maximum: 14, allow_null: true }]
+  });
+
+  person = new Person({id: 1, name: 'Attila the Hun'});
+  same(person.valid(), true);
+
+  person = new Person({id: 1, name: null});
+  same(person.valid(), true);
 });
 
 test("It should optionally validate length within a given range", function(){
-  same(true, false)
-});
+  var Person = new Class({
+    Extends: MooModel.Base,
+    Validations: [{ attribute: 'name', type: 'length', within: [10, 14], allow_null: true }]
+  });
 
-test("It should optionally validate length for a particular value", function(){
-  same(true, false)
+  person = new Person({id: 1, name: null});
+  same(person.valid(), true);
 });
